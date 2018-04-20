a3aed51bddc957160cd53f64da9890b6145e35f4
lucas <lucas@DESKTOP-6C6GKKU>
2018-04-20 12:26:00 +0200

    prima prova progetto

---
+++ ProgettoUniversita/src/main/java/com/app/universita/ProgettoUniversitaApplication.java
@@ -0,0 +1,12 @@
+package com.app.universita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoUniversitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoUniversitaApplication.class, args);
+	}
+}
